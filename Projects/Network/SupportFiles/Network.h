ce487a87e2c469a2f8d0e3df466c33481adfe16a
devming <cmk330@gmail.com>
2021-11-14 17:52:55 +0900

    [ chore ] Repository 프로젝트 네이밍 Network로 변경

---
+++ Projects/Network/SupportFiles/Network.h
@@ -0,0 +1,19 @@
+//
+//  Network.h
+//  Network
+//
+//  Created by devming on 2021/09/22.
+//  Copyright © 2021 com.666. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Network.
+FOUNDATION_EXPORT double NetworkVersionNumber;
+
+//! Project version string for Network.
+FOUNDATION_EXPORT const unsigned char NetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Network/PublicHeader.h>
+
+
