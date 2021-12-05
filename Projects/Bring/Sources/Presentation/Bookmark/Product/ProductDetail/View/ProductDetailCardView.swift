7db84b2f81c2df5afa56e1c627e8da61535cfb01
Cado.Avo <cado.avo@kakaopaycorp.com>
2021-12-05 17:19:47 +0900

    [ feat ] animation scroll view 작업중...

--- Projects/Bring/Sources/Presentation/Bookmark/Product/ProductDetail/View/ProductDetailCardView.swift
+++ Projects/Bring/Sources/Presentation/Bookmark/Product/ProductDetail/View/ProductDetailCardView.swift
@@ -22,12 +22,24 @@ struct ProductDetailCardView: View {
         "https://image.msscdn.net/images/goods_img/20190812/1115974/1115974_3_big.jpg",
         "https://image.msscdn.net/images/goods_img/20210112/1744960/1744960_3_500.jpg",
         "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
+        "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
+        "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
+        "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
+        "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
+        "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
+        "https://image.msscdn.net/images/goods_img/20210813/2064478/2064478_3_500.jpg",
         "https://image.msscdn.net/images/goods_img/20200225/1322019/1322019_8_500.jpg"
     ]
     var brandUrl: [String] = [
         "https://image.msscdn.net/display/images/2021/06/25/196e5d0096454623911ab0e4bfaedd51.png",
         "https://image.msscdn.net/display/images/2021/06/25/6e4431a19154401982ea35a9c040f320.png",
         "https://image.msscdn.net/images/brand/2019021211221000000099990.png",
+        "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
+        "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
+        "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
+        "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
+        "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
+        "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
         "https://image.msscdn.net/mfile_s01/_brand/free_medium/lafudgestore.png?202110061655",
         "https://image.musinsa.com/images/brand/2020050812382500000027035.png"
     ]
@@ -37,7 +49,6 @@ struct ProductDetailCardView: View {
         ZStack {
             //AsyncImage(url: URL(string: "https://your_image_url_address"))
             // iOS15이상이면 이렇게 바로 쓸수 있는것같네요
-            
             AsyncImage(url: URL(string: thumbnailUrls[index])){ phase in
                 switch phase {
                     case .empty:
